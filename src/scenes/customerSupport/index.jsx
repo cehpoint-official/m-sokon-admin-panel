d08aa417cf448d418c764e7fc5b5480819cad64a
Ashraful Islam <19103189@iubat.edu>
2023-09-19 20:53:01 +0600

    customer support section updated

--- src/scenes/customerSupport/index.jsx
+++ src/scenes/customerSupport/index.jsx
@@ -4,23 +4,83 @@ import {
   Button,
   Card,
   CardContent,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
   TextField,
   Typography,
 } from "@mui/material";
 
-// Component for handling customer inquiries and complaints
 const CustomerSupport = () => {
   const [ticket, setTicket] = useState("");
+  const [assignedStaff, setAssignedStaff] = useState(""); // State to store assigned staff member
+  const [inquiries, setInquiries] = useState([
+    {
+      content: "I have an issue with my order.",
+      assignedStaff: "staff1",
+      status: "Pending",
+    },
+    {
+      content: "I need assistance with my account.",
+      assignedStaff: "staff2",
+      status: "Pending",
+    },
+    {
+      content: "I received a damaged product.",
+      assignedStaff: "staff3",
+      status: "Resolved",
+    },
+  ]); // State to store customer inquiries
+  const [selectedInquiry, setSelectedInquiry] = useState(null);
+  const [isEditModalOpen, setEditModalOpen] = useState(false);
 
   const handleTicketChange = (event) => {
     setTicket(event.target.value);
   };
 
-  const submitTicket = () => {
-    // Code to submit the customer's inquiry or complaint
-    console.log("Submitted Ticket:", ticket);
+  const handleAssignedStaffChange = (event) => {
+    setAssignedStaff(event.target.value);
   };
 
+  const submitTicket = () => {
+    const newInquiry = {
+      content: ticket,
+      assignedStaff,
+      status: "Pending",
+    };
+
+    setInquiries([...inquiries, newInquiry]);
+    setTicket("");
+    setAssignedStaff("");
+  };
+
+  const resolveInquiry = (index) => {
+    const updatedInquiries = [...inquiries];
+    updatedInquiries[index].status = "Resolved";
+    setInquiries(updatedInquiries);
+  };
+
+  const openEditModal = (inquiry) => {
+    setSelectedInquiry(inquiry);
+    setEditModalOpen(true);
+  };
+
+  const closeEditModal = () => {
+    setSelectedInquiry(null);
+    setEditModalOpen(false);
+  };
   return (
     <Card>
       <CardContent>
@@ -35,16 +95,109 @@ const CustomerSupport = () => {
         >
           <TextField
             label="Customer Inquiry or Complaint"
+            color="info"
             value={ticket}
             onChange={handleTicketChange}
             multiline
             rows={4}
             variant="outlined"
           />
-          <Button variant="contained" color="primary" onClick={submitTicket}>
+          <FormControl variant="outlined">
+            <InputLabel id="assigned-staff-label">Assign Staff</InputLabel>
+            <Select
+              labelId="assigned-staff-label"
+              color="info"
+              id="assigned-staff"
+              value={assignedStaff}
+              onChange={handleAssignedStaffChange}
+              label="Assign Staff"
+            >
+              <MenuItem value="">
+                <em>None</em>
+              </MenuItem>
+              <MenuItem value="staff1">Staff 1</MenuItem>
+              <MenuItem value="staff2">Staff 2</MenuItem>
+              <MenuItem value="staff3">Staff 3</MenuItem>
+            </Select>
+          </FormControl>
+          <Button variant="contained" color="secondary" onClick={submitTicket}>
             Submit Ticket
           </Button>
         </Box>
+        <Typography variant="h6" mt={4}>
+          Customer Inquiries
+        </Typography>
+        <TableContainer component={Paper}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Inquiry</TableCell>
+                <TableCell>Assigned Staff</TableCell>
+                <TableCell>Status</TableCell>
+                <TableCell>Action</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {inquiries.map((inquiry, index) => (
+                <TableRow key={index}>
+                  <TableCell>{inquiry.content}</TableCell>
+                  <TableCell>{inquiry.assignedStaff}</TableCell>
+                  <TableCell>{inquiry.status}</TableCell>
+                  <TableCell>
+                    {inquiry.status === "Pending" && (
+                      <>
+                        <Button
+                          variant="outlined"
+                          color="info"
+                          onClick={() => resolveInquiry(index)}
+                        >
+                          Resolve
+                        </Button>
+                        <Button
+                          variant="outlined"
+                          color="secondary"
+                          style={{marginLeft:"16px"}}
+                          onClick={() => openEditModal(inquiry)}
+                        >
+                          Edit
+                        </Button>
+                      </>
+                    )}
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        {/* Inquiry Details Modal */}
+        <Dialog
+          open={isEditModalOpen}
+          onClose={closeEditModal}
+          maxWidth="md"
+          fullWidth
+        >
+          <DialogTitle>Inquiry Details</DialogTitle>
+          <DialogContent>
+            {selectedInquiry && (
+              <TextField
+                label="Inquiry"
+                value={selectedInquiry.content}
+                multiline
+                rows={4}
+                variant="outlined"
+                color="info"
+                style={{marginTop:"10px"}}
+                fullWidth
+                // disabled
+              />
+            )}
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={closeEditModal} color="warning">
+              Close
+            </Button>
+          </DialogActions>
+        </Dialog>
       </CardContent>
     </Card>
   );
